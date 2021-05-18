e2a0febe427a29ca3d05dd2f05217e01915d0bbb
kangming <kangming@sensetime.com>
2021-05-18 09:53:02 +0800

    fix: 修复行情排序组件数据异常的问题

--- src/screens/ticker.tsx
+++ src/screens/ticker.tsx
@@ -16,8 +16,11 @@ const SorterFunc = (data: any, sorter: any) => {
   if (!data || data.length === 0) {
     return [];
   }
+
   const {name: sortName, rule: sortRule} = sorter;
-  data.sort((a: any, b: any) => {
+  const sortData: any = [...data];
+
+  sortData.sort((a: any, b: any) => {
     let sortFieldValA, sortFieldValB;
     if (sortName === SortField.NAME) {
       sortFieldValA = a.basic.symbol;
@@ -31,15 +34,30 @@ const SorterFunc = (data: any, sorter: any) => {
       sortFieldValA = a.meta.P;
       sortFieldValB = b.meta.P;
     }
+
     if (sortRule === SortRule.ASC) {
-      return sortFieldValA - sortFieldValB ? 1 : -1;
+      if (
+        Number.isNaN(parseFloat(sortFieldValA)) &&
+        Number.isNaN(parseFloat(sortFieldValB))
+      ) {
+        return sortFieldValB > sortFieldValA ? 1 : -1;
+      }
+
+      return parseFloat(sortFieldValB) - parseFloat(sortFieldValA);
     }
     if (sortRule === SortRule.DESC) {
-      return sortFieldValB - sortFieldValA ? 1 : -1;
+      if (
+        Number.isNaN(parseFloat(sortFieldValA)) &&
+        Number.isNaN(parseFloat(sortFieldValB))
+      ) {
+        return sortFieldValA > sortFieldValB ? 1 : -1;
+      }
+
+      return parseFloat(sortFieldValA) - parseFloat(sortFieldValB);
     }
   });
 
-  return data;
+  return sortData;
 };
 
 const TickerItem = (props: any) => {
